980e7a5c10a3e937cd1cc0964801bcb9146ae00d
Fabryv <39920892+Fabry200@users.noreply.github.com>
2025-11-09 11:56:20 +0100

    Update Engine.py

--- Engine.py
+++ Engine.py
@@ -20,10 +20,9 @@ class Engine:
             for e,_,_ in self.user.embeddings[:3]:
                 score=self.score(embedding,e)
                 scores.append(score)
-            weights = np.exp(scores) / np.sum(np.exp(scores))
-            top3_scores = [e[2] for e in self.user.embeddings[:3]]
-            files[file] = np.dot(weights, top3_scores)           
-
+            combined = np.array(scores) * np.array([e[2] for e in self.user.embeddings[:3]])
+            weights = np.exp(combined) / np.sum(np.exp(combined))
+            files[file] = np.dot(weights, combined)
         return sorted(files, key=lambda video: files[video], reverse=True)[0]
 
     
